320826d35294ce1be9e360ddebc28dc4dac8d875
kondo takeshi <chaspy@users.noreply.github.com>
2022-12-17 21:55:12 +1100

    implement

--- typescript/26_Remove_Duplicates_from_Sorted_Array.ts
+++ typescript/26_Remove_Duplicates_from_Sorted_Array.ts
@@ -1,15 +1,38 @@
 function removeDuplicates(nums: number[]): number {
-  nums.forEach(function (value) {
-    console.log(value)
+  let arr: number[] = []
+  nums.forEach(function (value, index) {
+    console.log(`debug: value, index = ${value}, ${index}`)
+    console.log(`arr: ${arr}`)
+    if (index == 0) {
+      arr.push(value)
+    } else if (value == arr[arr.length - 1]) {
+      // do nothing
+      console.log(`skipped. now: ${arr}`)
+    } else {
+      arr.push(value)
+      console.log(`pushed. now: ${arr}`)
+    }
   })
-
-  return 1
+  console.log(`arr: ${arr}`)
+  return arr.length
 }
 
 // Example 1:
 // Input: nums = [1,1,2]
 // Output: 2, nums = [1,2,_]
 
+let nums = [1, 1, 2]
+let expectedNums = [1, 2]
+let k = removeDuplicates(nums)
+console.log(`k: ${k}`)
+console.log(`expectedNums.length: ${expectedNums.length}`)
+
 // Example 2:
 // Input: nums = [0,0,1,1,1,2,2,3,3,4]
 // Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+expectedNums = [0, 1, 2, 3, 4]
+k = removeDuplicates(nums)
+console.log(`k: ${k}`)
+console.log(`expectedNums.length: ${expectedNums.length}`)
