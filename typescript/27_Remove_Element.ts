67bb57861f79061cde3ae424055038c8c9fb3f26
kondo takeshi <chaspy@users.noreply.github.com>
2022-12-19 23:26:38 +1100

    27

---
+++ typescript/27_Remove_Element.ts
@@ -0,0 +1,36 @@
+function removeElement(nums: number[], val: number): number {
+  let k = 0
+  nums.forEach(function (value, index) {
+    if (index == 0) {
+      // do nothing
+      k += 1 // k = 1
+    } else if (nums.slice(0, k).includes(value)) {
+      // do nothing. skip since the number is duplicated.
+    } else {
+      nums[k] = value
+      k += 1
+    }
+  })
+  console.log(`nums: ${nums}`)
+  return k
+}
+
+// Example 1:
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+
+let nums = [1, 1, 2]
+let expectedNums = [1, 2]
+let k = removeElement(nums)
+console.log(`k: ${k}`)
+console.log(`expectedNums.length: ${expectedNums.length}`)
+
+// Example 2:
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+expectedNums = [0, 1, 2, 3, 4]
+k = removeElement(nums)
+console.log(`k: ${k}`)
+console.log(`expectedNums.length: ${expectedNums.length}`)
