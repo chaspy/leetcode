257303583ee358fa455b8638748fa94db4fc6841
kondo takeshi <chaspy@users.noreply.github.com>
2022-12-19 23:38:13 +1100

    27 ts

--- typescript/27_Remove_Element.ts
+++ typescript/27_Remove_Element.ts
@@ -1,11 +1,8 @@
 function removeElement(nums: number[], val: number): number {
   let k = 0
   nums.forEach(function (value, index) {
-    if (index == 0) {
-      // do nothing
-      k += 1 // k = 1
-    } else if (nums.slice(0, k).includes(value)) {
-      // do nothing. skip since the number is duplicated.
+    if (nums[index] == val) {
+      // do nothing. skip if val and nums[index] are the same
     } else {
       nums[k] = value
       k += 1
@@ -16,21 +13,21 @@ function removeElement(nums: number[], val: number): number {
 }
 
 // Example 1:
-// Input: nums = [1,1,2]
-// Output: 2, nums = [1,2,_]
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
 
-let nums = [1, 1, 2]
-let expectedNums = [1, 2]
-let k = removeElement(nums)
+let nums = [3, 2, 2, 3]
+let expectedNums = [2, 2]
+let k = removeElement(nums, 3)
 console.log(`k: ${k}`)
 console.log(`expectedNums.length: ${expectedNums.length}`)
 
 // Example 2:
-// Input: nums = [0,0,1,1,1,2,2,3,3,4]
-// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Input: nums = [0,1,2,2,3,0,4,2], val = 2
+// Output: 5, nums = [0,1,4,0,3,_,_,_]
 
-nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-expectedNums = [0, 1, 2, 3, 4]
-k = removeElement(nums)
+nums = [0, 1, 2, 2, 3, 0, 4, 2]
+expectedNums = [0, 1, 3, 0, 4]
+k = removeElement(nums, 2)
 console.log(`k: ${k}`)
 console.log(`expectedNums.length: ${expectedNums.length}`)
